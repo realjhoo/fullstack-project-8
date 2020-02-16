3b4a3959cb823a84262d9d7efdb09a6871317a62
Jerry L Hoover <jerry.hoover@ymail.com>
2020-02-15 21:27:35 -0600

    db retrieval working

--- app.js
+++ app.js
@@ -2,14 +2,12 @@
 // REQUIRES
 const express = require("express");
 const db = require("./db");
-const Book = require("./db/models/book").Book;
-const router = express.Router();
+const { Book } = require("./db").models;
 
 // VARS
 const app = express();
-let books = [];
 
-// app
+// app set and use
 app.set("view engine", "pug");
 app.use("/static", express.static("public"));
 
@@ -24,111 +22,57 @@ app.use("/static", express.static("public"));
   }
 })();
 
-// app.get(
-//   "/",
-//   asyncHandler(async (req, res) => {
-//     const books = await Book.findAll({
-//       order: [["title", "ASC"]]
-//     });
-//     res.render("index", { books: books });
-//   })
-// );
-
-// root rout = /books
-app.get("/", (req, res) => {
-  books = [
-    {
-      id: 1,
-      title: "Bad Book",
-      author: "Billy Bob Bobby Bill",
-      genre: "bad fiction",
-      year_published: 1996
-    },
-    {
-      id: 2,
-      title: "Good Book",
-      author: "Billy Bob Bobby Bill",
-      genre: "somewhat better fiction",
-      year_published: 1998
-    },
-    {
-      id: 3,
-      title: "Best Book",
-      author: "Billy Bob Bobby Bill",
-      genre: "Much improved fiction",
-      year_published: 2001
-    }
-  ];
-  res.render("index", {
-    books: books
-  });
-});
-
-app.get("/books", (req, res) => {
-  res.redirect("/");
-});
-
-// get /books/new
-app.get("/books/new", (req, res) => {
-  res.render("new-book", { title: "New Book" });
-});
-
-// post /books/new
-app.post("/books/new", (req, res) => {
-  res.render("new-book", { title: "New Book" });
-});
-
-// *********************************
-// TEMP BOOK-DETAIL
-app.get("/books/update", (req, res) => {
-  res.render("update-book", { title: "Book Title" });
-});
-
-// where id is the db id
-// get /books/:id
-// app.get("/books/:id", (req, res) => {
-//   // const book = await Book.findByPk(req.params.id);
-//   res.render("new-book", { title: book });
-// });
-
-// post /books/:id
-// app.post("/books/:id", (req, res) => {
-//   res.render("books:id");
-// });
-
-// post /books/:id/delete
-// app.get("/books/:id", (req, res) => {
-//   res.render("books/:id/delete");
-// });
-
-// testing route
+// =============== ROUTES ===============
+// works
 app.get(
-  "/books/:id",
+  "/",
   asyncHandler(async (req, res) => {
-    console.log("* * * This route is working * * *");
-    const book = await Book.findByPk(req.params.id);
-    console.log("BOOK BOOK BOOK BOOK " + book);
-    if (book) {
-      res.render("update-book");
-      console.log("We got this far.");
+    const books = await Book.findAll();
+    if (books) {
+      res.render("index", {
+        books: books,
+        id: books.id,
+        title: "SQL Library Manager"
+      });
     } else {
-      console.log("Nope, didnt work");
       res.sendStatus(404);
     }
   })
 );
 
-// /error
-// app.get("/error", (req, res) => {
-//   res.render("error");
+// redirect (needed?)
+// works
+// app.get("/books", (req, res) => {
+//   res.redirect("/");
 // });
-// ========== error handler ==========
+
+// NEW BOOKS
+// works
+app.get("/books/new", (req, res) => {
+  res.render("new-book", { title: "New Book" });
+});
+
+// Update Book
+app.get(
+  "/books/:id",
+  asyncHandler(async (req, res) => {
+    const book = await Book.findByPk(req.params.id);
+    if (book) {
+      res.render("update-book", { book: book, title: book.title });
+    } else {
+      res.sendStatus(404);
+    }
+  })
+);
+
+// =============== error handler ===============
 // set error
 app.use((req, res, next) => {
   const err = new Error("Cannot find that... sorry");
   err.status = 404;
   next(err);
 });
+
 // render error page
 app.use((err, req, res, next) => {
   err.status = err.status || 500;
@@ -138,6 +82,7 @@ app.use((err, req, res, next) => {
   console.log("Error: " + err.status + " " + err.message);
 });
 
+// server listening on port 3000
 const server = app.listen(3000, () => {
   console.log(
     `Server running on localhost port ${server.address().port} at ` + showTime()
--- db/index.js
+++ db/index.js
@@ -2,7 +2,8 @@ const Sequelize = require("sequelize");
 
 const sequelize = new Sequelize({
   dialect: "sqlite",
-  storage: "library.db"
+  storage: "library.db",
+  logging: false
 });
 
 const db = {
